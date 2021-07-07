8d3036d6b9c9ce249ca1f231358a732293c0dd74
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-07-07 21:46:45 +0530

    [Sairam]Added:Ability to find whether the employee is absent or present

---
+++ EmployeeWage.js
@@ -0,0 +1,9 @@
+const is_Absent = 0
+let check = Math.floor(Math.random() * 10) % 2
+if(check == is_Absent){
+    console.log("Employee is absent")
+}
+else{
+    console.log("Employee is present")
+}
+
