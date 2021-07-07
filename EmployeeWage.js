0ce511802b244553c97cf9ab3462dbede007b8ec
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-07-07 22:39:07 +0530

    [Sairam]Added:Ability to calculate the empwage of the whether the parttime or full time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,35 @@
-const is_Absent = 0
-let check = Math.floor(Math.random() * 10) % 2
-if(check == is_Absent){
+///UC-1 Check for present or absent
+{
+    const is_Absent = 0
+    let check = Math.floor(Math.random() * 10) % 2
+    if(check == is_Absent)
+    {
     console.log("Employee is absent")
-}
-else{
+    }
+    else
+    {
     console.log("Employee is present")
+    }
 }
+///Calculating Empwage
+const is_fulltime=1
+const is_parttime=2
+const full_timehr=8
+const part_timehr=4
+const emp_perhr=20
+    let emphr=0
+    let check=Math.floor(Math.random() * 10) %3
+    switch(check){
+        case is_fulltime:
+            emphr=full_timehr;
+            break;
+            case is_parttime:
+                emphr=part_timehr;
+                break;
+                default:
+                    emphr=0;
+    }
+    let empwage=emphr*emp_perhr;
+    console.log("the empwage is:"+empwage)
+
 
